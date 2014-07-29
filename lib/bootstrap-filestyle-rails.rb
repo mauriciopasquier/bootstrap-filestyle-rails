3657752b4eaecadee244747da95ff7c1a8c0198a
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2014-07-29 02:40:47 -0300

    Coding style: change double to single quotes

--- lib/bootstrap-filestyle-rails.rb
+++ lib/bootstrap-filestyle-rails.rb
@@ -1,13 +1,11 @@
-require "rails"
-require "bootstrap-filestyle-rails/version"
+require 'rails'
+require 'bootstrap-filestyle-rails/version'
 
 # Add bootstrap-filestyle to the asset pipeline
 module BootstrapFilestyleRails
-
-  if ::Rails.version.to_s < "3.1"
-    require "bootstrap-filestyle-rails/railtie"
+  if ::Rails.version.to_s < '3.1'
+    require 'bootstrap-filestyle-rails/railtie'
   else
-    require "bootstrap-filestyle-rails/engine"
+    require 'bootstrap-filestyle-rails/engine'
   end
-
 end
